93c2be23b18b9c8f0f781e7f21ef6d7d94496943
other50g <other50g@live.jp>
2015-12-08 16:40:44 +0900

    毎分ファイルを送信するスクリプト

--- script/test.sh
+++ script/test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-# scpコマンドでVMにファイルを送る -Pオプションはポート番号指定
-scp -P portnumber hoge.txt username@ipaddress:VMPath
+#時間間隔でファイルを送信する
+while true ; do
+  # dateコマンドで現在の日時を取得
+  date +"%H:%M:%S"
+  # scpコマンドでVMにファイルを送る -Pオプションはポート番号指定
+  scp -P 1234 file username@ipaddress:VMPath
+  # 1分ごとにファイル送信
+  sleep 60
+done
